fbb3e5a8179a34bab13f5ed943237fda07450e66
JohnLi139 <malkon@mail.ru>
2023-07-20 15:09:47 +0000

    modified

--- storage-numjobs.sh
+++ storage-numjobs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#for THREADS in `seq 4 4 $MAXTHREADS`
+                                        # в качестве изменяемой переменной используется кол-во параллельных потоков (numjobs)
+TIME=60                                 # время замера одного профиля
+SLEEP=5                                 # пауза между замерами
+MAXTHREADS=256                          # макс. кол-во параллельных потоков, д.б. кратно 8, рекомендуемые значения от 32 до 256
+DISK=/dev/vdb                           # имена дисков для тестирования - разделитель ":"
 
-TIME=60
-SLEEP=5
-MAXTHREADS=32
-DISK=/dev/vdb
-
-# ------------ 1 disk -- bloсk=4K -- 100% random read -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+# -------- №1 -- bloсk=4K -- 100% random read -- iodepth=1 -----------------------
+for ((p=1; p<=7; p++))
 do
   NAME=1d-4k-randread-1-$p
   fio \
@@ -29,8 +28,30 @@ do
   sleep $SLEEP
 done
 
-# ------------ 1 disk -- block=4K -- 100% random write -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+for p in `seq 8 8 $MAXTHREADS`
+do
+  NAME=1d-4k-randread-1-$p
+  fio \
+  --size=10g \
+  --direct=1 \
+  --buffered=0 \
+  --time_based \
+  --runtime=$TIME \
+  --ramp_time=5 \
+  --ioengine=libaio \
+  --group_reporting \
+  --name=$NAME \
+  --filename=$DISK \
+  --bs=4k \
+  --rw=randread \
+  --iodepth=1 \
+  --numjobs=$p \
+  --output=./out/$NAME.txt
+  sleep $SLEEP
+done
+
+# -------- №2 -- block=4K -- 100% random write -- iodepth=1 -----------------------
+for ((p=1; p<=7; p++))
 do
   NAME=1d-4k-randwrite-1-$p
   fio \
@@ -52,8 +73,30 @@ do
   sleep $SLEEP
 done
 
-# ------------ 1 disk -- block=64K -- 100% sequential read -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+for p in `seq 8 8 $MAXTHREADS`
+do
+  NAME=1d-4k-randwrite-1-$p
+  fio \
+  --size=10g \
+  --direct=1 \
+  --buffered=0 \
+  --time_based \
+  --runtime=$TIME \
+  --ramp_time=5 \
+  --ioengine=libaio \
+  --group_reporting \
+  --name=$NAME \
+  --filename=$DISK \
+  --bs=4k \
+  --rw=randwrite \
+  --iodepth=1 \
+  --numjobs=$p \
+  --output=./out/$NAME.txt
+  sleep $SLEEP
+done
+
+# -------- №3 -- block=64K -- 100% sequential read -- iodepth=1 -----------------------
+for ((p=1; p<=7; p++))
 do
   NAME=1d-64k-read-1-$p
   fio \
@@ -75,8 +118,30 @@ do
   sleep $SLEEP
 done
 
-# ------------ 1 disk -- block=64K -- 100% sequential write -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+for p in `seq 8 8 $MAXTHREADS`
+do
+  NAME=1d-64k-read-1-$p
+  fio \
+  --size=10g \
+  --direct=1 \
+  --buffered=0 \
+  --time_based \
+  --runtime=$TIME \
+  --ramp_time=5 \
+  --ioengine=libaio \
+  --group_reporting \
+  --name=$NAME \
+  --filename=$DISK \
+  --bs=64k \
+  --rw=read \
+  --iodepth=1 \
+  --numjobs=$p \
+  --output=./out/$NAME.txt
+  sleep $SLEEP
+done
+
+# -------- №4 -- block=64K -- 100% sequential write -- iodepth=1 -----------------------
+for ((p=1; p<=7; p++))
 do
   NAME=1d-64k-write-1-$p
   fio \
@@ -98,10 +163,9 @@ do
   sleep $SLEEP
 done
 
-# ------------ 1 disk -- block=8K -- 50% random read 50% random write -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+for p in `seq 8 8 $MAXTHREADS`
 do
-  NAME=1d-8k-randrw-1-$p
+  NAME=1d-64k-write-1-$p
   fio \
   --size=10g \
   --direct=1 \
@@ -113,18 +177,18 @@ do
   --group_reporting \
   --name=$NAME \
   --filename=$DISK \
-  --bs=8k \
-  --rw=randrw \
+  --bs=64k \
+  --rw=write \
   --iodepth=1 \
   --numjobs=$p \
   --output=./out/$NAME.txt
   sleep $SLEEP
 done
 
-# ------------ 1 disk -- block=32K -- 50% sequential read 50% sequential write -- iodepth=1 -- variable numjobs -----------------------
-for ((p=1; p<=$MAXTHREADS; p++))
+# -------- №5 -- block=32K -- 100% sequential read -- iodepth=1 -----------------------
+for ((p=1; p<=7; p++))
 do
-  NAME=1d-32k-rw-1-$p
+  NAME=1d-32k-read-1-$p
   fio \
   --size=10g \
   --direct=1 \
@@ -137,7 +201,29 @@ do
   --name=$NAME \
   --filename=$DISK \
   --bs=32k \
-  --rw=rw \
+  --rw=read \
+  --iodepth=1 \
+  --numjobs=$p \
+  --output=./out/$NAME.txt
+  sleep $SLEEP
+done
+
+for p in `seq 8 8 $MAXTHREADS`
+do
+  NAME=1d-32k-read-1-$p
+  fio \
+  --size=10g \
+  --direct=1 \
+  --buffered=0 \
+  --time_based \
+  --runtime=$TIME \
+  --ramp_time=5 \
+  --ioengine=libaio \
+  --group_reporting \
+  --name=$NAME \
+  --filename=$DISK \
+  --bs=32k \
+  --rw=read \
   --iodepth=1 \
   --numjobs=$p \
   --output=./out/$NAME.txt
