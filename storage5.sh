a3265db2526ceb9210d3d4ba97bd876c098b1119
JohnLi139 <malkon@mail.ru>
2023-09-04 16:17:47 +0300

    Added storage5.sh which test profiles progon51-progon72

---
+++ storage5.sh
@@ -0,0 +1,43 @@
+fio ./ini/progon51.ini --output=./out/progon51.txt
+sleep 10
+fio ./ini/progon52.ini --output=./out/progon52.txt
+sleep 10
+fio ./ini/progon53.ini --output=./out/progon53.txt
+sleep 10
+fio ./ini/progon54.ini --output=./out/progon54.txt
+sleep 10
+fio ./ini/progon55.ini --output=./out/progon55.txt
+sleep 10
+fio ./ini/progon56.ini --output=./out/progon56.txt
+sleep 10
+fio ./ini/progon57.ini --output=./out/progon57.txt
+sleep 10
+fio ./ini/progon58.ini --output=./out/progon58.txt
+sleep 10
+fio ./ini/progon59.ini --output=./out/progon59.txt
+sleep 10
+fio ./ini/progon60.ini --output=./out/progon60.txt
+sleep 10
+fio ./ini/progon61.ini --output=./out/progon61.txt
+sleep 10
+fio ./ini/progon62.ini --output=./out/progon62.txt
+sleep 10
+fio ./ini/progon63.ini --output=./out/progon63.txt
+sleep 10
+fio ./ini/progon64.ini --output=./out/progon64.txt
+sleep 10
+fio ./ini/progon65.ini --output=./out/progon65.txt
+sleep 10
+fio ./ini/progon66.ini --output=./out/progon66.txt
+sleep 10
+fio ./ini/progon67.ini --output=./out/progon67.txt
+sleep 10
+fio ./ini/progon68.ini --output=./out/progon68.txt
+sleep 10
+fio ./ini/progon69.ini --output=./out/progon69.txt
+sleep 10
+fio ./ini/progon70.ini --output=./out/progon70.txt
+sleep 10
+fio ./ini/progon71.ini --output=./out/progon71.txt
+sleep 10
+fio ./ini/progon72.ini --output=./out/progon72.txt
