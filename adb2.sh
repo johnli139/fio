2c99216b6f8bc83c45211bd289638735df9000ff
JohnLi139 <malkon@mail.ru>
2023-10-05 15:12:58 +0000

    added ID in file names

--- adb2.sh
+++ adb2.sh
@@ -2,11 +2,15 @@
 
 TIME=60                                  # время замера одного профиля
 SLEEP=10                                 # пауза между замерами
-FILE=/data8/fio                          # имена файлов для тестирования - разделитель ":", необходимо указывать файл для каждой mountpoint
+FILE=/data1/fio                          # имена файлов для тестирования - разделитель файлов ":"
+#FILE=/data1/fio:/data2/fio
+#FILE=/data1/fio:/data2/fio:/data3/fio
+#FILE=/data1/fio:/data2/fio:/data3/fio:/data4/fio
 IODEPTH=1                                # кол-во IO, посылаемых в ОС без подтверждения их получения от ОС
 NUMJOBS=64                               # кол-во параллельных задач (процессов) 
+ID=$(date +%s)
 
-OUT="./$(hostname)_$(date +%d.%m.%Y_%H:%M:%S).csv"
+OUT="./$ID-$(hostname).csv"
 if [ -f "$OUT" ]; then
    rm -f $OUT
 fi
@@ -14,15 +18,13 @@ fi
 echo Hostname,$(hostname) >> $OUT
 echo Date,$(date +%d.%m.%Y" "%H:%M:%S) >> $OUT
 echo Fio version,$(fio --version) >> $OUT
-echo Test run time,$TIME >> $OUT
-echo Tested file,$FILE >> $OUT
 echo >> $OUT
-echo BLOCK SIZE,PROFILE,IODEPTH,NUMJOBS,,IOPS,THROUGHPUT,LATENCY >> $OUT
+echo TESTED FILES,TEST TIME,BLOCK SIZE,PROFILE,IODEPTH,NUMJOBS,,IOPS,THROUGHPUT,LATENCY >> $OUT
 
 #---------------------512K-WRITE----------------------------------------------------------------------------#
 BS=512k                                  # размер блока
 RW=write                                 # тип теста: randread/randwrite/read/write
-NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
+NAME=$ID-$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
 
   fio \
   --size=10g \
@@ -41,7 +43,7 @@ NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
   --numjobs=$NUMJOBS \
   --output=./out/$NAME.txt
 
-echo $BS,$RW,$IODEPTH,$NUMJOBS,,\
+echo $FILE,$TIME,$BS,$RW,$IODEPTH,$NUMJOBS,,\
 $(awk -F "=" '/IOPS=/  { print  $2 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
 $(awk -F "=" '/IOPS=/  { print  $3 }' ./out/$NAME.txt | awk -F "M" ' { print $1 }'),\
 $(awk -F "=" '/\slat.*avg/  { print  $4 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
@@ -51,7 +53,7 @@ sleep $SLEEP
 #---------------------512K-READ----------------------------------------------------------------------------#
 BS=512k                                  # размер блока
 RW=read                                  # тип теста: randread/randwrite/read/write
-NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
+NAME=$ID-$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
 
   fio \
   --size=10g \
@@ -70,7 +72,7 @@ NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
   --numjobs=$NUMJOBS \
   --output=./out/$NAME.txt
 
-echo $BS,$RW,$IODEPTH,$NUMJOBS,,\
+echo $FILE,$TIME,$BS,$RW,$IODEPTH,$NUMJOBS,,\
 $(awk -F "=" '/IOPS=/  { print  $2 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
 $(awk -F "=" '/IOPS=/  { print  $3 }' ./out/$NAME.txt | awk -F "M" ' { print $1 }'),\
 $(awk -F "=" '/\slat.*avg/  { print  $4 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
@@ -80,7 +82,7 @@ sleep $SLEEP
 #---------------------32K-WRITE----------------------------------------------------------------------------#
 BS=32k                                   # размер блока
 RW=write                                 # тип теста: randread/randwrite/read/write
-NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
+NAME=$ID-$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
 
   fio \
   --size=10g \
@@ -99,7 +101,7 @@ NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
   --numjobs=$NUMJOBS \
   --output=./out/$NAME.txt
 
-echo $BS,$RW,$IODEPTH,$NUMJOBS,,\
+echo $FILE,$TIME,$BS,$RW,$IODEPTH,$NUMJOBS,,\
 $(awk -F "=" '/IOPS=/  { print  $2 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
 $(awk -F "=" '/IOPS=/  { print  $3 }' ./out/$NAME.txt | awk -F "M" ' { print $1 }'),\
 $(awk -F "=" '/\slat.*avg/  { print  $4 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
@@ -109,7 +111,7 @@ sleep $SLEEP
 #---------------------32K-READ----------------------------------------------------------------------------#
 BS=32k                                   # размер блока
 RW=read                                  # тип теста: randread/randwrite/read/write
-NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
+NAME=$ID-$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
 
   fio \
   --size=10g \
@@ -128,7 +130,7 @@ NAME=$BS-$RW-$IODEPTH-$NUMJOBS           # имя выходного файла
   --numjobs=$NUMJOBS \
   --output=./out/$NAME.txt
 
-echo $BS,$RW,$IODEPTH,$NUMJOBS,,\
+echo $FILE,$TIME,$BS,$RW,$IODEPTH,$NUMJOBS,,\
 $(awk -F "=" '/IOPS=/  { print  $2 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
 $(awk -F "=" '/IOPS=/  { print  $3 }' ./out/$NAME.txt | awk -F "M" ' { print $1 }'),\
 $(awk -F "=" '/\slat.*avg/  { print  $4 }' ./out/$NAME.txt | awk -F "," ' { print $1 }'),\
